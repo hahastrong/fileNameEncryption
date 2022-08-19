3d026381b57659af9ca02b32848993aed3e1c848
hahastrong <sgq1148376413@163.com>
2022-08-19 20:41:07 +0800

    add dir encrypt

--- fileName_test.go
+++ fileName_test.go
@@ -20,3 +20,22 @@ func TestValidateFileName(t *testing.T) {
 		t.Fatal("validate false")
 	}
 }
+
+func TestValidateLastDirFileName(t *testing.T) {
+	filename := "hell.dfd/wv20220817/SC.mp4"
+
+	filenameExt, err := GenerateLastDirFileName(filename)
+	if err != nil {
+		t.Fatalf("failed to generate filename, err: %v", err)
+	}
+
+	t.Logf("%s -> %s", filename, filenameExt)
+
+	flag, err := ValidateLastDirFileName(filenameExt)
+	if err != nil {
+		t.Fatalf("failed to parse filename, err: %v", err)
+	}
+	if !flag {
+		t.Fatal("validate false")
+	}
+}
